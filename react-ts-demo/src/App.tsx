fc96c29834689c6addfa5cf10b2e806787708e12
umer-shehzad <umer.work.96@gmail.com>
2024-04-25 12:43:15 +0500

    Object Type props - ts

--- react-ts-demo/src/App.tsx
+++ react-ts-demo/src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css'
 import Greet from './components/Greet'
+import Person from './components/Person'
 
 function App() {
+  const person = {
+    first: 'Abdullah',
+    last: 'Sarim'
+  }
+
   return (
     <>
       <Greet name="Umer" messsageCount={10} isLoggedIn={false} />
+      <Person name={person}/>
     </>
   )
 }
